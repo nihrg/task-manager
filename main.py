d8b4ddc3bb22be0578adb255f1140581de83d764
nihrg <ng3106@stern.nyu.edu>
2025-01-04 15:05:04 -0500

    Simple CLI To-Do List in Python

---
+++ main.py
@@ -0,0 +1,12 @@
+# task-manager
+# A simple command-line to-do list application written in Python.
+
+import os
+
+TASKS_FILE = 'tasks.txt'
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    with open(TASKS_FILE, 'r') as file:
+        return [task.strip() for task in file.readlines()]
